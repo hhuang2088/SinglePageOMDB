b8487152cfc29ee1a5fc1bba11969d570fb0c884
Henry Huang <hhuang2088@gmail.com>
2014-06-10 16:14:52 -0700

    basic functionality down

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+$(document).ready(function(){
+  
+  $('#searchButton').on("click", function(e){
+    e.preventDefault();
+    $('#results').html("")
+    var search = $('#searchTerm').val();
+    
+    var searchResult = $.ajax({
+      url: "http://www.omdbapi.com/",
+      async: true,
+      dataType: "json",
+      data: { s: search }
+
+    })
+    searchResult.done(function(results){
+      $.each(results["Search"], function(index, movie) {
+        $('#results').append('<li data-imdbID='+movie["imdbID"]+'>' + movie["Title"] + '</li>');
+      })
+    });
+  });
+
+  $('#results').delegate('li', 'click', function(e){
+      e.preventDefault();
+      var id = $(e.target).data("imdbid")
+      var moviePoster = $.ajax({
+        url: "http://www.omdbapi.com",
+        async: true,
+        dataType: "json",
+        data: { i: id}
+      })
+      moviePoster.done(function(data){
+        $('#poster').html("");
+        $('#poster').append("<img src='"+ data["Poster"]+"'/>");
+      })
+  });
+
+
+});
